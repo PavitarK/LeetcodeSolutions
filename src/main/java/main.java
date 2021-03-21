417d2bb851069591af75046a9e43e92454bcad93
PavitarK <me@pavitar.ca>
2021-03-20 17:04:16 -0700

    added comments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,7 +4,7 @@ import static solutions.ArrangingCoins.arrangeCoins;
 
 public class main {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 20;
         HappyNumber happy = new HappyNumber(); 
 
         System.out.println(happy.isHappy(n));
--- src/main/java/solutions/HappyNumber.java
+++ src/main/java/solutions/HappyNumber.java
@@ -14,27 +14,33 @@ public class HappyNumber {
             HashMap<Integer, Integer> allValues = new HashMap<Integer, Integer>();
 
             while(check_happy){
+                //perform modulus and divisions of number until 0
                 for(n_mod = new_n; n_mod > 0; n_mod = n_mod /10){
                     remainder = n_mod %10;
                     sum += remainder * remainder;
                 }
+
+                //check if sum = 1
                 if(sum == 1){
                     check_happy = false;
                 }
                 else{
                     check_happy = true;
+
+                    // to check if in an infinite loop make sure we aren't getting the same sum twice
                     allValues.put(i, sum);
                     for(int j = 0; j < i; j++){
                         compare = allValues.get(j);
                         if(compare == sum){
-                            return false;
+                            return false; //if duplicate found we know not happy
                         }
                     }
+                    // if not in infinite loop update flags
                     new_n = sum;
                     sum = 0;
                     i++;
                 }
             }
-            return !check_happy;
+            return true;
         }
     }
